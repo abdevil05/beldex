e6c07ecffa092b1addf41ed71422fa5fe2ef37f6
victor-tucci <victor.tucci@beldex.io>
2025-04-21 14:44:16 +0530

    RPC: SUBMIT_TRANSACTION new rpc update - Rename endpoint from "send_raw_transaction" to "submit_transaction"   and rename the RPC struct similarly.
    - Rename tx_as_hex to just tx, and start accepting it as hex, base64, or
      raw bytes.  (Transactions formats are consistent enough that we can
      easily tell which one we have from the first couple bytes).
    
    - Drop `do_not_relay` parameter as I can see no purpose to ever
      submitting a transaction to a daemon that shouldn't be relayed.  (The
      only use I can see would be to try to exploit a PoW network with
      double-spends).
    
    - Drop `do_sanity_checks` parameter; the checks done here should be (and
      are!) done by the wallet, not the beldexd receiving the completed
      transaction.

--- src/rpc/core_rpc_server.cpp
+++ src/rpc/core_rpc_server.cpp
@@ -120,7 +120,7 @@ namespace cryptonote::rpc {
       static_assert(!FIXME_has_nested_response_v<RPC>);
 
       cmd->invoke = [](rpc_request&& request, core_rpc_server& server) -> rpc_command::result_type {
-        RPC rpc;
+        RPC rpc{};
         try {
           if (auto body = request.body_view()) {
             if (body->front() == 'd') { // Looks like a bt dict
@@ -1220,88 +1220,72 @@ namespace cryptonote::rpc {
    spent.response["spent_status"] = std::move(spent_status);
   }
   //------------------------------------------------------------------------------------------------------------------------------
-  SEND_RAW_TX::response core_rpc_server::invoke(SEND_RAW_TX::request&& req, rpc_context context)
+  void core_rpc_server::invoke(SUBMIT_TRANSACTION& tx, rpc_context context)
   {
-    SEND_RAW_TX::response res{};
-
-    PERF_TIMER(on_send_raw_tx);
-    if (use_bootstrap_daemon_if_necessary<SEND_RAW_TX>(req, res))
+    PERF_TIMER(on_submit_transaction);
+    /*
+    if (use_bootstrap_daemon_if_necessary<SUBMIT_TRANSACTION>(req, res))
       return res;
+    */
 
-    CHECK_CORE_READY();
-
-    if (!oxenc::is_hex(req.tx_as_hex))    {
-      LOG_PRINT_L0("[on_send_raw_tx]: Failed to parse tx from hexbuff: " << req.tx_as_hex);
-      res.status = "Failed";
-      return res;
+    if (!check_core_ready()) {
+      tx.response["status"] = STATUS_BUSY;
+      return;
     }
-    auto tx_blob = oxenc::from_hex(req.tx_as_hex);
 
-    if (req.flash)
+    if (tx.request.flash)
     {
-      auto future = m_core.handle_flash_tx(tx_blob);
+      auto future = m_core.handle_flash_tx(tx.request.tx);
+      // FIXME: blocking here for 10s is nasty; we need to stash this request and come back to it
+      // when the flash tx result comes back, and wait for longer (maybe 30s).
+      //
+      // FIXME 2: on timeout, we should check the mempool to see if it arrived that way so that we
+      // return success if it got out to the network, even if we didn't get the flash quorum reply
+      // for some reason.
       auto status = future.wait_for(10s);
       if (status != std::future_status::ready) {
-        res.status = "Failed";
-        res.reason = "Flash quorum timeout";
-        res.flash_status = flash_result::timeout;
-        return res;
+        tx.response["status"] = STATUS_FAILED;
+        tx.response["reason"] = "Flash quorum timeout";
+        tx.response["flash_status"] = flash_result::timeout;
+        return;
       }
 
       try {
         auto result = future.get();
-        res.flash_status = result.first;
+        tx.response["flash_status"] = result.first;
         if (result.first == flash_result::accepted) {
-          res.status = STATUS_OK;
+          tx.response["status"] = STATUS_OK;
         } else {
-          res.status = "Failed";
-          res.reason = !result.second.empty() ? result.second : result.first == flash_result::timeout ? "Flash quorum timeout" : "Transaction rejected by flash quorum";
+          tx.response["status"] = STATUS_FAILED;
+          tx.response["reason"] = !result.second.empty() ? result.second : result.first == flash_result::timeout ? "Flash quorum timeout" : "Transaction rejected by flash quorum";
         }
       } catch (const std::exception &e) {
-        res.flash_status = flash_result::rejected;
-        res.status = "Failed";
-        res.reason = std::string{"Transaction failed: "} + e.what();
+        tx.response["flash_status"] = flash_result::rejected;
+        tx.response["status"] = STATUS_FAILED;
+        tx.response["reason"] = "Transaction failed: "s + e.what();
       }
-      return res;
+      return;
     }
 
     tx_verification_context tvc{};
-    if(!m_core.handle_incoming_tx(tx_blob, tvc, tx_pool_options::new_tx(req.do_not_relay)) || tvc.m_verifivation_failed)
+    if (!m_core.handle_incoming_tx(tx.request.tx, tvc, tx_pool_options::new_tx()) || tvc.m_verifivation_failed || !tvc.m_should_be_relayed)
     {
-      const vote_verification_context &vvc = tvc.m_vote_ctx;
-      res.status          = "Failed";
-      std::string reason  = print_tx_verification_context  (tvc);
-      reason             += print_vote_verification_context(vvc);
-      res.tvc             = tvc;
-      const std::string punctuation = res.reason.empty() ? "" : ": ";
-      if (tvc.m_verifivation_failed)
-      {
-        LOG_PRINT_L0("[on_send_raw_tx]: tx verification failed" << punctuation << reason);
-      }
-      else
-      {
-        LOG_PRINT_L0("[on_send_raw_tx]: Failed to process tx" << punctuation << reason);
-      }
-      return res;
+      tx.response["status"] = STATUS_FAILED;
+      auto reason = print_tx_verification_context(tvc);
+      LOG_PRINT_L0("[on_send_raw_tx]: " << (tvc.m_verifivation_failed ? "tx verification failed" : "Failed to process tx") << reason);
+      tx.response["reason"] = std::move(reason);
+      tx.response["reason_codes"] = tx_verification_failure_codes(tvc);
+      return;
     }
 
-    if(!tvc.m_should_be_relayed)
-    {
-      LOG_PRINT_L0("[on_send_raw_tx]: tx accepted, but not relayed");
-      res.reason = "Not relayed";
-      res.not_relayed = true;
-      res.status = STATUS_OK;
-      return res;
-    }
-
+    // Why is is the RPC handler's responsibility to tell the p2p protocol to relay a transaction?!
     NOTIFY_NEW_TRANSACTIONS::request r{};
-    r.txs.push_back(tx_blob);
+    r.txs.push_back(std::move(tx.request.tx));
     cryptonote_connection_context fake_context{};
     m_core.get_protocol()->relay_transactions(r, fake_context);
 
-    //TODO: make sure that tx has reached other nodes here, probably wait to receive reflections from other nodes
-    res.status = STATUS_OK;
-    return res;
+    tx.response["status"] = STATUS_OK;
+    return;
   }
   //------------------------------------------------------------------------------------------------------------------------------
   void core_rpc_server::invoke(START_MINING& start_mining, rpc_context context)
