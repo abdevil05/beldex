0c16cb8f95c5a232a99ec2c0ad58f083d505974e
abdevil05 <abdevil@beldex.io>
2026-09-01 18:18:32 +0530

    fix(tests): use inline constexpr protocol limits
    Define the asynchronous protocol-handler test constants as inline static constexpr values to avoid linkage and modern C++ compilation problems.

--- tests/unit_tests/epee_levin_protocol_handler_async.cpp
+++ tests/unit_tests/epee_levin_protocol_handler_async.cpp
@@ -181,8 +181,8 @@ namespace
   class async_protocol_handler_test : public ::testing::Test
   {
   public:
-    const static uint64_t invoke_timeout = 5 * 1000;
-    const static size_t max_packet_size = 10 * 1024 * 1024;
+    inline static constexpr uint64_t invoke_timeout = 5 * 1000;
+    inline static constexpr size_t max_packet_size = 10 * 1024 * 1024;
 
     typedef std::unique_ptr<test_connection> test_connection_ptr;
 
